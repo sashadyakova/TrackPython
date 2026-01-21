203df7eabb9d6785506057aa89ef10404984a1d9
sashadyakova <sasha.dyakova2425@gmail.com>
2026-01-21 22:12:07 +0500

    Лаборатроная 4. Дьякова А.Е.

---
+++ task_2.py
@@ -0,0 +1,24 @@
+# TODO импортировать необходимые модули
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open(INPUT_FILENAME, mode='r', newline='', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = list(reader)  # TODO считать содержимое csv файла
+
+    with open(OUTPUT_FILENAME, mode='w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)  # TODO Сериализовать в файл с отступами равными 4
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
